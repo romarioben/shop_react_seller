3d387aa2015ec8e40fc4975b57d6d842efbfda73
romario <benoitgandonou@gmail.com>
2025-08-01 09:09:54 +0100

    login-post-01-08-2025-deux

---
+++ src/pages/Profile.jsx
@@ -0,0 +1,13 @@
+import { Button } from 'bootstrap'
+import React from 'react'
+
+function Profile() {
+  return (
+    <div>
+      <p>This is the Profile page.</p>
+      <Button variant="primary">Get profile data</Button>
+    </div>
+  )
+}
+
+export default Profile
